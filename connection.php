c5260679cc02d2658b34737e1b6587b2123237c9
atangana <salomon7atangana@gmail.com>
2021-05-14 15:08:52 +0200

    Login.php
    ho sviluppato il file login per non fa più eseguire il file install.php
    
    Co-Authored-By: jacopoColozzi <81474943+jacopoColozzi@users.noreply.github.com>

--- connection.php
+++ connection.php
@@ -5,9 +5,9 @@
     if ($db_creato == false) { // Questa variebile va settata prima di "require_once("connection.php");". 
                                 //Di default è falso e verrà quindi creato il db
         echo "connessione no db";
-        $connessione = new mysqli("localhost", "phpmyadmin", "7Tonnerres");
+        $connessione = new mysqli("localhost", "root", "");
     } else {
-        $connessione = new mysqli("localhost", "phpmyadmin", "7Tonnerres", $nomeDB);
+        $connessione = new mysqli("localhost", "root", "", $nomeDB);
     }
     if (mysqli_errno($connessione)) {
         printf("Oops, abbiamo problemi con la connessione al db: %s\n", mysqli_error($connessione));
